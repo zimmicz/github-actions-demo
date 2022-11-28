effc1b17711b3d74c75567211016b33a57b016eb
Michal Zimmermann <zimmicz@gmail.com>
2022-11-28 08:56:47 +0100

    feat: main

--- .github/actions/semver-check.js
+++ .github/actions/semver-check.js
@@ -2,8 +2,6 @@ const semver = require("semver");
 const [version] = process.argv.slice(2);
 const package = require("../../package.json");
 
-console.log("package:", package.package);
-
 if (!semver.valid(version)) {
   throw new Error(`Version ${version} doesn't adhere to semver standard.`);
 }
