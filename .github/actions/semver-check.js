34f75e327d69ff74d5a18092dbeb6af07246172a
Michal Zimmermann <zimmicz@gmail.com>
2022-11-28 08:45:54 +0100

    feat: main

--- .github/actions/semver-check.js
+++ .github/actions/semver-check.js
@@ -2,22 +2,18 @@ const semver = require("semver");
 const [version] = process.argv.slice(2);
 const { package } = require("../../package.json");
 
-const semverCheck = () => {
-  if (!semver.valid(version)) {
-    throw new Error(`Version ${version} doesn't adhere to semver standard.`);
-  }
+if (!semver.valid(version)) {
+  throw new Error(`Version ${version} doesn't adhere to semver standard.`);
+}
 
-  if (!semver.valid(package.version)) {
-    throw new Error(
-      `Current package version ${package.version} doesn't adhere to semver standard.`
-    );
-  }
+if (!semver.valid(package.version)) {
+  throw new Error(
+    `Current package version ${package.version} doesn't adhere to semver standard.`
+  );
+}
 
-  if (!semver.gt(version, package.version)) {
-    throw new Error(
-      `Version ${version} is not greater than the current version ${package.version}.`
-    );
-  }
-};
-
-module.exports = { semverCheck };
+if (!semver.gt(version, package.version)) {
+  throw new Error(
+    `Version ${version} is not greater than the current version ${package.version}.`
+  );
+}
