9729bc81bad1cfd4a82ebeac83f65fc2a0720ce8
Michal Zimmermann <zimmicz@gmail.com>
2022-11-28 08:24:43 +0100

    feat: main

--- .github/actions/semver-check.js
+++ .github/actions/semver-check.js
@@ -1,3 +1,8 @@
+const semver = require("semver");
+const [version] = process.argv.slice(2);
+const package = require("../../package.json");
+console.log("package:", package);
+
 const semverCheck = () => {
   console.log("semverCheck");
 };
