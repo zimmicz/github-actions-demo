b2055bc15d1676fef4c7eb102ad5c6a261dcbd74
Michal Zimmermann <zimmicz@gmail.com>
2023-02-03 09:26:18 +0100

    feat: button story

--- webapp/src/button/button.stories.tsx
+++ webapp/src/button/button.stories.tsx
@@ -3,3 +3,4 @@ import { Button } from './button';
 export const Default = () => <Button />;
 export const Default1 = () => <Button />;
 export const Default2 = () => <Button />;
+export const Default3 = () => <Button />;
